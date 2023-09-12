214d1c15e08284f94a6621b1d1eb47639a0fddd1
Lucas Beleboni <123699186+Beleboni-dev@users.noreply.github.com>
2023-09-12 16:19:56 -0300

    feat: last commit

--- scripts/submitFormMiddle.js
+++ scripts/submitFormMiddle.js
@@ -4,14 +4,29 @@ const finalizeRegistrationMiddle = document.getElementById(
 );
 
 let whatsappMiddle = "";
+let brandNameMiddle = "";
+
+document.querySelectorAll(".step-middle input").forEach((input) => {
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (nextStep1ButtonMiddle.style.display === "none") {
+        finalizeRegistrationMiddle.click();
+      } else {
+        nextStep1ButtonMiddle.click();
+      }
+    }
+  });
+});
 
 nextStep1ButtonMiddle.addEventListener("click", (e) => {
   e.preventDefault();
-  const brandNameMiddle = document.getElementById(
+  brandNameMiddle = document.getElementById(
     "form-input-brand-middle"
   ).value;
   const nameMiddle = document.getElementById("form-input-name-middle").value;
   whatsappMiddle = document.getElementById("form-input-whatsapp-middle").value;
+  const stepTitle = document.getElementById("step-1-title-middle");
   const lineOfBusinessMiddle = document.getElementById(
     "form-input-line-of-business-middle"
   ).value;
@@ -65,9 +80,14 @@ nextStep1ButtonMiddle.addEventListener("click", (e) => {
       if (response.status === 200) {
         const step1Middle = document.getElementById("step-middle-1");
         const step2Middle = document.getElementById("step-middle-2");
+
+          stepTitle.textContent =
+            "Preencha esses dados opcionais para agilizar o seu atendimento";
+
         step1Middle.classList.add("hide-display");
         step2Middle.classList.remove("hide-display");
         showToast("Primeira etapa concluída!");
+                 nextStep1ButtonMiddle.style.display = "none";
       } else {
         console.error("Erro na integração com o Piperun.");
       }
@@ -136,7 +156,7 @@ finalizeRegistrationMiddle.addEventListener("click", (e) => {
     },
     leads: [leadStep2Middle],
   };
-  console.log(dataToSendStep2Middle);
+
   const endpointMiddle =
     "https://app.pipe.run/webservice/integradorJson?hash=f1e34340-bdad-49a4-a40a-9d3eb26e2328";
 
