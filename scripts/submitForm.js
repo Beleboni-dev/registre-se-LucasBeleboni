4361836dfbe52e7760e24fb0e13c5de3af5f6103
fregossi <146000886+fregossi@users.noreply.github.com>
2023-09-25 15:59:23 -0300

    Update submitForm.js

--- scripts/submitForm.js
+++ scripts/submitForm.js
@@ -103,7 +103,7 @@ const formattedName = formatName(name);
           loading.classList.add("hide-display");
         }, 1000); 
       } else {
-        console.error("Erro na integração com o site. Por favor, preencha novamente.");
+        console.error("Erro na integração com o Piperun.");
       }
     })
     .catch((error) => {
@@ -194,7 +194,7 @@ finalizeRegistration.addEventListener("click", (e) => {
 
         `;
       } else {
-        console.error("Erro na integração com o site. Por favor, preencha novamente.");
+        console.error("Erro na integração com o Piperun.");
       }
     })
     .catch((error) => {
